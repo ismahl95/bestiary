52e6be2adddb1ccf59c5ebfa5ab18a410eef59fd
= <=>
2024-04-05 19:29:28 +0200

    Initial Commit

---
+++ src/main/java/com/bestiary/bestiary/BestiaryApplication.java
@@ -0,0 +1,13 @@
+package com.bestiary.bestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestiaryApplication.class, args);
+	}
+
+}
