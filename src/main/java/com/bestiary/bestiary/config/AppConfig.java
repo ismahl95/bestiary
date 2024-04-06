cf8922c8717c0d0231848e37552a288d5373fddb
= <=>
2024-04-06 11:00:24 +0200

    feat: base config class

---
+++ src/main/java/com/bestiary/bestiary/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.bestiary.bestiary.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.bestiary.bestiary")
+public class AppConfig {
+  
+}
